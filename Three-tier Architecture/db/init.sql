31a29e9cc9eec0ef1acfce1aa1ecb02384ead28e
tejasKhamkar2211 <tejaskhamkar23@gmail.com>
2024-12-29 10:21:32 +0530

    Create init.sql

---
+++ Three-tier Architecture/db/init.sql
@@ -0,0 +1,5 @@
+create database facebook;
+use facebook;
+create table users(id int primary key auto_increment, fname varchar(100), lname varchar(100));
+insert into users (fname, lname) values("shri" ,"Ganesh")
+
